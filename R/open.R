79a19aa785e876c17efd770dddc8831114836847
Jeanette <jclark@nceas.ucsb.edu>
2022-01-14 08:21:16 -0800

    rewrite open functions to convert to duckdb to get around arrow join issue on windows
    I have require() here instead of requireNamespace since it works, will continue to try to get requireNamespace to work...

--- R/open.R
+++ R/open.R
@@ -3,15 +3,24 @@
 #' Open the survey data stored in the package
 #'
 #' @return survey An arrow object that can be treated like a data.frame, with survey data
+#' @importFrom magrittr %>%
+#' @import dplyr
 #' @export
 
 open_survey <- function(){
+    
     if (!(dir.exists(rappdirs::user_cache_dir("deltafish")))){
         stop("Cache directory does not exist. Try running `create_fish_db().")
     }
-    survey <- arrow::open_dataset(file.path(rappdirs::user_cache_dir("deltafish"), "survey"))
     
-    if (length(survey$files) == 0) stop("No survey dataset found. Run create_fish_db() first.")
+    if (!require(dplyr, quietly = TRUE)) {
+        stop("please install dplyr first")
+    }
+    
+    survey <- arrow::open_dataset(file.path(rappdirs::user_cache_dir("deltafish"), "survey")) %>% 
+        arrow::to_duckdb(auto_disconnect = TRUE)
+    
+    #if (length(survey$files) == 0) stop("No survey dataset found. Run create_fish_db() first.")
     
     return(survey)
 }
@@ -21,15 +30,23 @@ open_survey <- function(){
 #' Open the survey data stored in the package
 #'
 #' @return fish An arrow object that can be treated like a data.frame, with fish data
+#' @importFrom magrittr %>%
+#' @import dplyr
 #' @export
 #' 
 open_fish <- function(){
     if (!(dir.exists(rappdirs::user_cache_dir("deltafish")))){
         stop("Cache directory does not exist. Try running `create_fish_db().")
     }
-    fish  <- arrow::open_dataset(file.path(rappdirs::user_cache_dir("deltafish"), "fish"))
     
-    if (length(fish$files) == 0) stop("No fish dataset found. Run create_fish_db() first.")
+    if (!require(dplyr, quietly = TRUE)) {
+        stop("please install dplyr first")
+    }
+    
+    fish  <- arrow::open_dataset(file.path(rappdirs::user_cache_dir("deltafish"), "fish")) %>% 
+        arrow::to_duckdb(auto_disconnect = TRUE)
+    
+    #if (length(fish$files) == 0) stop("No fish dataset found. Run create_fish_db() first.")
     
     return(fish)
 }
@@ -39,6 +56,8 @@ open_fish <- function(){
 #' Connect to the length conversion data stored in the package
 #'
 #' @return fish An arrow object that can be treated like a data.frame, with length conversion data
+#' @importFrom magrittr %>%
+#' @import dplyr
 #' @export
 
 open_length_conv <- function(){
@@ -46,9 +65,14 @@ open_length_conv <- function(){
         stop("Cache directory does not exist. Try running `create_fish_db().")
     }
     
-    lconv  <- arrow::open_dataset(file.path(rappdirs::user_cache_dir("deltafish"), "length_conversion"))
+    if (!require(dplyr, quietly = TRUE)) {
+        stop("please install dplyr first")
+    }
     
-    if (length(lconv$files) == 0) stop("No length conversion dataset found. Run create_fish_db() first.")
+    lconv  <- arrow::open_dataset(file.path(rappdirs::user_cache_dir("deltafish"), "length_conversion")) %>% 
+        arrow::to_duckdb(auto_disconnect = TRUE)
+    
+    #if (length(lconv$files) == 0) stop("No length conversion dataset found. Run create_fish_db() first.")
     
     return(lconv)
 }
