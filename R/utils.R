b95c89bf348b38fd9198d1c6935ccdc938bf6218
Jeanette Clark <jclark@nceas.ucsb.edu>
2022-05-02 12:31:43 -0700

    rework how the trycatch is written

--- R/utils.R
+++ R/utils.R
@@ -8,6 +8,14 @@
 #'
 show_cache <- function() dir(rappdirs::user_cache_dir("deltafish"), full.names = TRUE)
 
+
+
+file_remove_tryer <- function(x){
+    tryCatch({file.remove},
+         error = function(x){print(x); message("If you get a permissions error, try restarting R and then clearing your cache.")}
+    )
+}
+
 #' Clear cached deltafish files (internal)
 #'
 #' This function removes all cached files associated with the package
@@ -20,12 +28,11 @@ clear_cache_f <- function(cache_dir){
     files <- dir(rappdirs::user_cache_dir(cache_dir), full.names = TRUE, recursive = TRUE)
     if (length(files) > 0){
         message("Removing existing cache.")
-        tryCatch({lapply(files, file.remove)},
-                 error = function(x){print(x); message("If you get a permissions error, try restarting R and then clearing your cache.")
-                     }
-                 )
+        lapply(files, file_remove_tryer)
         return(NULL)
-    } else message("No cache to remove.")
+    } else {
+        message("No cache to remove.")
+    }
 
 } 
 
