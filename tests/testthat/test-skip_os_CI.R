9cb22df25a855a609a8e354054d8ee76f7d53382
sbashevkin <sam.bashevkin@deltacouncil.ca.gov>
2022-05-16 15:43:37 -0700

    Improving accuracy of skip_os_CI test

--- tests/testthat/test-skip_os_CI.R
+++ tests/testthat/test-skip_os_CI.R
@@ -1,16 +1,16 @@
 test_that("skip_os_CI works", {
     # Windows, no CI
-    withr::local_envvar(CI = "false")
+    withr::local_envvar(CI = "")
     mockery::stub(skip_os_CI, "Sys.info", function() list(sysname="windows"))
     expect_condition(skip_os_CI("darwin"), NA, class = "skip") 
     
     # Mac, no CI
-    withr::local_envvar(CI = "false")
+    withr::local_envvar(CI = "")
     mockery::stub(skip_os_CI, "Sys.info", function() list(sysname="darwin"))
     expect_condition(skip_os_CI("darwin"), NA, class = "skip") 
    
     # Linux, no CI
-    withr::local_envvar(CI = "false")
+    withr::local_envvar(CI = "")
     mockery::stub(skip_os_CI, "Sys.info", function() list(sysname="linux"))
     expect_condition(skip_os_CI("darwin"), NA, class = "skip") 
     
