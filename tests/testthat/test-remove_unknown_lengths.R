96997d35d33012f9289211c10784c22d709f7599
sbashevkin <sam.bashevkin@deltacouncil.ca.gov>
2022-05-17 09:44:16 -0700

    Trying to increase efficiency of `remove_unknown_lengths` test to run on GH actions windows and linux

--- tests/testthat/test-remove_unknown_lengths.R
+++ tests/testthat/test-remove_unknown_lengths.R
@@ -1,28 +1,42 @@
-# Skip these test if on CI, and not using mac
-skip_os_CI("darwin")
+
 
 surv <- open_survey()
 fish <- open_fish()
-length_conv <- open_length_conv()
 
 df_removed_nu <- dplyr::inner_join(fish, surv) %>%
-    remove_unknown_lengths(univariate = FALSE) %>% 
+    select(SampleID, Taxa, Count, Length)%>%
+    compute() %>%
+    remove_unknown_lengths(univariate = FALSE) %>%  
+    select(Length, Count)%>%
+    compute()%>%
     summarise(N=n(), Length_NA=sum(as.integer(is.na(Length) & Count!=0), na.rm=T))%>%
     collect()
 
-df_removed_u <- dplyr::inner_join(fish, surv) %>%
+gc()
+
+df_removed_u <- dplyr::inner_join(fish, surv)%>%
+    select(SampleID, Taxa, Count, Length)%>%
+    compute() %>%
     remove_unknown_lengths(univariate = TRUE)  %>% 
+    select(Length, Count)%>%
+    compute()%>%
     summarise(N=n(), Length_NA=sum(as.integer(is.na(Length) & Count!=0), na.rm=T))%>% 
     collect()
 
+gc()
+
 df_full <- dplyr::inner_join(fish, surv)  %>% 
+    select(Length, Count)%>%
+    compute()%>%
     summarise(N=n(), Length_NA=sum(as.integer(is.na(Length) & Count!=0), na.rm=T))%>% 
     collect()
+
+gc()
 
 test_that("remove_unknown_lengths removes rows as expected", {
     
-    expect_true(df_full$N > df_removed_u$N & df_removed_u$N > df_removed_nu$N)
-    expect_true(df_removed_nu$Length_NA == 0 & df_removed_u$Length_NA == 0)
+    expect_true(all(df_full$N > df_removed_u$N & df_removed_u$N > df_removed_nu$N))
+    expect_true(all(df_removed_nu$Length_NA == 0 & df_removed_u$Length_NA == 0))
 
 })
 
