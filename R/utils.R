3809982606ab1028813b6afa3e6dd953414b6627
Jeanette Clark <jclark@nceas.ucsb.edu>
2022-05-20 14:51:06 -0700

    add utility functions
    show revision number for data stored in cache, get latest EDI revision, and get EDI pids associated with a revision

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,19 @@
+#' Show revision number of cached files
+#'
+#' This function returns the EDI revision number of the cached data.
+#'
+#' @param (char) The cache directory, by default set to deltafish for most use cases.
+#' @return (char) The revision number in the cache
+#' @export
+#'
+show_cached_revision <- function(cache_dir = "deltafish") {
+    rev <- scan(file.path(rappdirs::user_cache_dir(cache_dir), "revision.txt"),
+                what = "char",
+                quiet = TRUE)
+    return(rev)
+}
+
+
 #' Show list of cached deltafish files
 #'
 #' This function returns a list of files cached for the package.
@@ -8,14 +24,6 @@
 #'
 show_cache <- function() dir(rappdirs::user_cache_dir("deltafish"), full.names = TRUE)
 
-
-
-file_remove_tryer <- function(x){
-    tryCatch({file.remove(x)},
-         error = function(y){print(y); message("If you get a permissions error, try restarting R and then clearing your cache."); return(invisible(NULL))},
-         warning = function(y){print(y); message("If you get a permissions error, try restarting R and then clearing your cache."); return(invisible(NULL))}
-    )
-}
 
 #' Clear cached deltafish files (internal)
 #'
@@ -48,6 +56,16 @@ clear_cache_f <- function(cache_dir){
 #'
 clear_cache <- function(){
     clear_cache_f(cache_dir = "deltafish")
+}
+
+
+file_remove_tryer <- function(x){
+    
+    tryCatch({file.remove(x)},
+             error = function(y){print(y); message("If you get a permissions error, try restarting R and then clearing your cache."); return(invisible(NULL))},
+             warning = function(y){print(y); message("If you get a permissions error, try restarting R and then clearing your cache."); return(invisible(NULL))}
+    )
+    
 }
 
 #' Skip tests helper
@@ -95,4 +113,35 @@ skip_os_ci<-function(os, check_ci=TRUE){
     # Otherwise (not running chosen os and either A) not checking ci or B) checking ci and on ci) skip
     msg<-paste0("Test skipped, ", ifelse(check_ci, "when using CI, ", ""), "only run on ", os)
     testthat::skip(msg)
+}
+
+#' get EDI pids helper
+#'
+#' Get EDI pids needed for the package from a package ID
+#' 
+#' @noRd
+#'
+get_edi_pids <- function(package_id){
+    res <- EDIutils::read_data_entity_names(packageId = package_id)
+    
+    len <- grep("[L|l]ength.conversion", res$entityName)
+    com <- grep("[C|c]ompress", res$entityName)
+    
+    pids <- list(length = res$entityId[len],
+                 compressed = res$entityId[com])
+    return(pids)
+}
+
+
+#' get EDI latest version
+#'
+#' Get EDI latest version helper
+#' 
+#' @noRd
+#'
+get_latest_EDI_revision <- function(){
+    t <- EDIutils::list_data_package_revisions("edi", "1075")
+    t_final <- t[length(t)]
+    
+    return(paste0("edi.1075.", as.character(t_final)))
 }
